eb337a6982308aa734931b0a0cea88234562574c
Jan Lucas <jan-lucas@hetnet.nl>
2016-05-06 11:47:45 +0200

    refactoring mostly

--- receiver.py
+++ receiver.py
@@ -3,6 +3,8 @@
 import socket
 import datetime
 
+FORMAT = "jpg"
+
 soc = socket.socket()
 soc.bind(("", 8000))
 soc.listen(5)
@@ -10,15 +12,15 @@ while True:
     sock, adrr_info = soc.accept()
     recv_bytes = -1
     timestamp = str(datetime.datetime.now())
-    file = open("images/"+timestamp, "wb")
+    file_name = "images/{0}.{1}".format(timestamp, FORMAT)
+    file = open(file_name, "wb")
     buffer = bytearray(4096)
     total_bytes = 0
     while recv_bytes != 0:
         recv_bytes = sock.recv_into(buffer)
         file.write(buffer[:recv_bytes])
         total_bytes += recv_bytes
-
-print(total_bytes)
+    print("{0} bytes written to file {1}".format(total_bytes, file_name))
 file.close()
 soc.close()
 
--- webcam.py
+++ webcam.py
@@ -3,14 +3,14 @@
 from subprocess import Popen, PIPE, DEVNULL
 import socket
 
-HOST = "10.9.210.20"
+HOST = "localhost"
 PORT = 8000
 while True:
     input("Press enter to take a picture")
-    process = Popen(["fswebcam", "--quiet", "--no-banner", "-r", "1600x900", "--jpeg", "95",  "-"], stdout=PIPE, stderr=DEVNULL)
+    process = Popen(["fswebcam", "--quiet", "--no-banner", "-r", "1600x900", "--jpeg", "95", "-"], stdout=PIPE, stderr=DEVNULL)
     image = process.stdout.read()
-    print(image[:20])
-    print(len(image))
+    image_len = str(len(image))
+    print("{0} bytes captured".format(image_len))
     soc = socket.socket()
     soc.connect((HOST, PORT))
     soc.sendall(image)
