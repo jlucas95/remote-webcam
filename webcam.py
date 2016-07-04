fdebce08bcfb5010a1752c751abd81545ef2fc7b
Jan Lucas <jan-lucas@hetnet.nl>
2016-07-04 10:34:11 +0200

    Adds SSL

--- webcam.py
+++ webcam.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3.4
+"""
+Creates a picture using the webcam and sends it over a socket
+"""
 
 from subprocess import Popen, PIPE, DEVNULL
 import socket
+import ssl
+HOST = "IP"
+PORT = 0
 
-HOST = "localhost"
-PORT = 8000
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
 while True:
     input("Press enter to take a picture")
-    process = Popen(["fswebcam", "--quiet", "--no-banner", "-r", "1600x900", "--jpeg", "95", "-"], stdout=PIPE, stderr=DEVNULL)
+    process = Popen(["fswebcam", "--quiet", "--no-banner", "-r", "1600x900", "--jpeg", "95", "-"],
+		stdout=PIPE, stderr=DEVNULL)
     image = process.stdout.read()
     image_len = str(len(image))
     print("{0} bytes captured".format(image_len))
     soc = socket.socket()
-    soc.connect((HOST, PORT))
-    soc.sendall(image)
-    soc.close()
+    sslsoc = context.wrap_socket(soc)
+    sslsoc.connect((HOST, PORT))
+    sslsoc.sendall(image)
+    sslsoc.close()
